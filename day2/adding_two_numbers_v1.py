63a938668c37ae2991904a5ac1155feeaa81f61a
VitorTakiuti <vitor.takiuti@hotmail.com>
2025-07-01 02:05:33 -0300

    Add 2 different versions of adding two numbers to day 2

---
+++ day2/adding_two_numbers_v1.py
@@ -0,0 +1,7 @@
+
+number1 = input("Choose the first number: ")
+number2 = input("Choose the second number: ")
+
+adding_two_numbers = int(number1) + int(number2)
+
+print(f"This is the result: {adding_two_numbers}")
---
+++ day2/adding_two_numbers_v2.py
@@ -0,0 +1,15 @@
+def add(number1, number2):
+    adding_two_numbers = number1 + number2
+    print(adding_two_numbers)
+    return adding_two_numbers
+
+#add(1, 4)
+
+def add_two_num(): 
+    number1 = int(input("Choose the first number: "))
+    number2 = int(input("Choose the second number: "))
+    adding_two_numbers = number1 + number2
+    print(adding_two_numbers)
+    return adding_two_numbers
+
+add_two_num()
