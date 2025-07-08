33d857f1f85b0da32d21789b3b02a6c254871016
VitorTakiuti <vitor.takiuti@hotmail.com>
2025-07-07 23:59:17 -0300

    Day14 Building a Prime Number Checker

--- day14/prime_number_checker.py
+++ day14/prime_number_checker.py
@@ -6,7 +6,7 @@ is_primenumber = True
 if number <= 1:
     is_primenumber = False
     
-for i in range(2, int(number ** o.5) + 1):
+for i in range(2, int(number ** 0.5) + 1):
     if number % i == 0:
         is_primenumber = False
         break
